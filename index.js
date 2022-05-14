82b21792838140b383dd633b75d5b3180e5cb084
Matt Crecraft <mcrecraft@gmail.com>
2022-05-14 17:20:27 -0500

    Boom

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-function boom(){
+function boom() {
   document.querySelector("#message").textContent = "BOOM"
 }
+const button = document.querySelector("button")
+button.addEventListener("click", event => {
+  boom()
+})
